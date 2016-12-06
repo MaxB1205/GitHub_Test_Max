29d924daac89adc26d77b749741cd871d6cb1ae7
Mark Rogers <moonshinerboy1337@gmail.com>
2016-12-06 11:02:53 +0100

    Update GitHub_Test_Max.java

--- src/github_test_max/GitHub_Test_Max.java
+++ src/github_test_max/GitHub_Test_Max.java
@@ -10,13 +10,15 @@ package github_test_max;
  * @author bloch.maximilian
  */
 public class GitHub_Test_Max {
-
+	
+String a = "Hello";
+String b = "World!";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-	    System.out.println("Hello World");
-	    System.out.println("Hello World2");
+	    System.out.println(a);
+	    System.out.println(b);
         // TODO code application logic here
     }
     
